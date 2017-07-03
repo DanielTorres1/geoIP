be4d5f0952b086f69e3ebb2b3c9a5a034b6053f2
DanielTorres1 <daniel.torres@owasp.org>
2017-07-02 20:04:54 -0400

    update

--- geoip.pl
+++ geoip.pl
@@ -2,16 +2,21 @@
 use geoip;
 use JSON qw( decode_json ); 
 use Data::Dumper;
+use Try::Tiny;
 
 my $geo_ip = geoip->new();
 
 my $ip = $ARGV[0];
 $json_response = $geo_ip->get_data(ip => $ip);
-
-my $decoded_json = decode_json($json_response);
-#print Dumper $decoded_json;
-my $isp = $decoded_json->{'isp'};
-my $regionName = $decoded_json->{'regionName'};
-    
-print "$regionName,$isp \n";
-
+my $decoded_json;
+try {
+	$decoded_json = decode_json($json_response);
+	#print Dumper $decoded_json;
+	my $isp = $decoded_json->{'isp'};
+	my $regionName = $decoded_json->{'regionName'};
+	my $country = $decoded_json->{'country'};
+	print "$regionName ($country),$isp \n";
+} 
+catch {   
+	print "";
+};
